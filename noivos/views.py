e9a5718ab20e05920dc362cab2da7f44796d3ba4
Jadson-Jose <jadsonjosesilva@gmail.com>
2024-11-04 21:31:53 -0300

    Segundo commit

--- noivos/views.py
+++ noivos/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def home(request):
-     return HttpResponse('Olá Mundo!')
+     if request.method == "GET":
+          return render(request, 'home.html')
+     elif request.method == "POST":
+          nome_presente = request.POST.get('nome_presente')
+          preco = request.POST.get('preco')
+          importancia = int(request.POST.get('importancia'))
+          foto = request.FILES.get('foto')
+          
+          if importancia < 1 or importancia > 5:
+               return redirect('home')
+          return HttpResponse("Teste")
